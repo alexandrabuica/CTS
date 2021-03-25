e6cc97240f91f32b2be6ca63a6493c64c9ef0ddd
alexandrabuica <alexandrabuica99@gmail.com>
2021-03-25 11:20:22 +0200

    third commit

---
+++ sem05_cts/src/ro/ase/cts/prototype/Program.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.prototype;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Reteta reteta1 = new Reteta("Apa oxigenata", 50);
+		Reteta reteta2 = (Reteta)reteta1.copiaza();
+		System.out.println(reteta1);
+		System.out.println(reteta2);
+	}
+
+}
---
+++ sem05_cts/src/ro/ase/cts/prototype/RetetaMedicament.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.prototype;
+
+public interface RetetaMedicament extends Cloneable{
+
+	public RetetaMedicament copiaza();
+}

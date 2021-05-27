789c4e273c2e9fb5a9766c54393659f8d92cb5a0
alexandrabuica <alexandrabuica99@gmail.com>
2021-05-27 11:19:38 +0300

    second commit

--- sem13_cts/src/ro/ase/cts/teste/TestGrupa.java
+++ sem13_cts/src/ro/ase/cts/teste/TestGrupa.java
@@ -2,12 +2,71 @@ package ro.ase.cts.teste;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
 
 public class TestGrupa {
- 
+	private Grupa grupa;
+	
+	@Before
+	public void setUp() {
+		//10 studenti cu cate 3 note
+		grupa = new Grupa(1077);
+		for (int i=0; i<10; i++) {
+			Student s1 = new Student();
+			s1.adaugaNota(5);
+			s1.adaugaNota(10);
+			s1.adaugaNota(4);
+			grupa.adaugaStudent(s1);
+		}
+	}
+	
+	@Test
+	public void testGetPromovabilitateRight() {
+		Student studentMarcel = new Student("Marcel");
+		studentMarcel.adaugaNota(10);
+		studentMarcel.adaugaNota(10);
+		
+		Student studentMaria = new Student("Maria");
+		studentMaria.adaugaNota(10);
+		studentMaria.adaugaNota(10);
+		
+		grupa.adaugaStudent(studentMarcel);
+		grupa.adaugaStudent(studentMaria);
+		assertEquals(0.16, grupa.getPromovabilitate(), 0.1); 
+	}
+	
+	@Test
+	public void testGetPromovabilitateLowerBoundary() { 
+		assertEquals(0, grupa.getPromovabilitate(), 0.1);
+	}
+	
+	@Test
+	public void testGetPromovabilitateUpperBoundary() { 
+		Grupa grupa = new Grupa(1076);
+		for (int i=0; i<10; i++) {
+			Student student = new Student("Ion");
+			student.adaugaNota(7);
+			student.adaugaNota(9);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(1, grupa.getPromovabilitate(), 0.1);
+	}
+	 
+	@Test(expected = IllegalArgumentException.class )
+	public void testGetPromovabilitateError() {
+		Grupa grupa = new Grupa(1000);
+		grupa.getPromovabilitate();
+	}
+	
+	@Test(timeout=500)
+	public void testGetPromovabilitatePerformance() {
+		grupa.getPromovabilitate();
+	}
+	
 	@Test
 	public void testConstructorRight() {
 		Grupa grupa = new Grupa(1077);
